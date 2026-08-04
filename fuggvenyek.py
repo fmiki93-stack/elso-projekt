fe8abda2ecda24813ca51cca3b2c2532038caa9b
fmiki93-stack <f.miki93@gmail.com>
2026-08-04 10:06:47 +0200

    Add blank lines for improved readability in fuggvenyek.py

--- fuggvenyek.py
+++ fuggvenyek.py
@@ -4,6 +4,8 @@ def fuggveny1():
 def fuggveny2(uzenet):
     print("Ez a második függvény!")
 
+
+
 def fuggveny3(szam1, szam2):
     eredmeny = szam1 + szam2
     print("Az eredmény: ", eredmeny)    
