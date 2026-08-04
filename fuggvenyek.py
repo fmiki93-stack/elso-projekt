ff8cbba621ca05ab52909f427a7af6b5399ec3c1
fmiki93-stack <f.miki93@gmail.com>
2026-08-04 10:05:08 +0200

    Add functions to fuggvenyek.py and update print statements in main.py

---
+++ fuggvenyek.py
@@ -0,0 +1,9 @@
+def fuggveny1():
+    print("Ez az első függvény!")
+
+def fuggveny2(uzenet):
+    print("Ez a második függvény!")
+
+def fuggveny3(szam1, szam2):
+    eredmeny = szam1 + szam2
+    print("Az eredmény: ", eredmeny)    
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print("Sziasztok, ez egy python prgram!")
+
+print("Ez egy új sor!")
+
+print("Ez egy új sor!")
